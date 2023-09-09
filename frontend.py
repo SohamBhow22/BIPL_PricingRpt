e2b049fa6369266f87716fc5b4c34e2228b56967
Soham Bhowmick <32588339+SohamBhow22@users.noreply.github.com>
2023-09-09 20:04:56 +0530

    frontend_v5.py

--- frontend.py
+++ frontend.py
@@ -1,4 +1,4 @@
-import getdata_v6
+import getdata_v7
 import createviz_v5
 
 import oracledb as ora
@@ -8,8 +8,8 @@ import pandas as pd
 from PIL import Image
 
 
-st.title("Data Viz")
-st.text("Hello GeeksForGeeks!!!")
+st.title("Pricing Data Visualizations")
+#st.text("Hello GeeksForGeeks!!!")
 tab1, tab2 = st.tabs(["Forward Pricing", "🗃 Data"])
 
 ora.init_oracle_client()
@@ -17,13 +17,14 @@ cs = "localhost:1521/XE"
 #cs = "117.248.251.123:1521/XE"
 configpath = "C:\oraclexe\app\oracle\product\11.2.0\server\network\ADMIN"
 
-"""data = getdata_v6.getData_FwdPrice(cs,configpath)
-graph_data = data.filter(['Curve Date', 'Price'])
-print(graph_data)"""
+dates = getdata_v7.getData_DistinctEntDate(cs, configpath)
+cnames = getdata_v7.getData_DistinctCurveName(cs, configpath)
+physfin = getdata_v7.getData_DistinctPhysFin(cs, configpath)
 
-dates = getdata_v6.getData_DistinctEntDate(cs, configpath)
-cnames = getdata_v6.getData_DistinctCurveName(cs, configpath)
-physfin = getdata_v6.getData_DistinctPhysFin(cs, configpath)
+def fetchfwddata(cs, configpath, curvename, date, physfin):
+    data = getdata_v7.getData_FwdPrice(cs, configpath, curvename, date, physfin)
+    graph_data = data.filter(['Curve Date', 'Price'])
+    print(graph_data)
 
 
 with tab1:
@@ -37,8 +38,8 @@ with tab1:
         physfin = st.selectbox("Physical_Financial: (not mandatory)", physfin)
         st.write("Physical_Financial Curve Name selected is: ", physfin)
 
-        if (st.button("Generate Graph", key = "gengraph", use_container_width=False)):
-            st.write("Hello")
+        if (st.button("Generate Graph", key='gengraph', use_container_width=False)):
+            #st.write("Hello")
             if(curvename == "All" and date != "None"):
                 st.error("Please select a Curve Name", icon="🚨")
             elif(curvename != "All" and date == "None"):
@@ -48,14 +49,18 @@ with tab1:
             elif(curvename != "All" and date != "None"):
                 st.toast('Graph will be generated soon', icon="ℹ️")
                 #st.write("Graph for ", curvename, "Noice")
-                data = getdata_v6.getData_FwdPrice(cs, configpath, curvename, date, physfin)
+                data = getdata_v7.getData_FwdPrice(cs, configpath, curvename, date, physfin)
                 graph_data = data.filter(['Curve Date', 'Price'])
                 print(graph_data)
-                st.line_chart(data=graph_data, x='Curve Date', y='Price', use_container_width=True)
-                st.write("getdata_v6.getData_FwdPrice has been called")
+                if not graph_data.empty:
+                    st.line_chart(data=graph_data, x='Curve Date', y='Price', use_container_width=True)
+                else:
+                    st.error("No data available for the selected Curve name and Date", icon="🚨")
+                #st.write("getdata_v6.getData_FwdPrice has been called")
                 st.toast('This is a success message!', icon="✅")            
 
-        
-
+    #on_click=fetchfwddata, args=[cs, configpath, curvename, date, physfin],    
+    
+#st.write(st.session_state)
     
 #tab2.write(graph_data)
