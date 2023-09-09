3287094370fd30021539458a576b9d59e02d361e
Soham Bhowmick <32588339+SohamBhow22@users.noreply.github.com>
2023-09-09 20:06:22 +0530

    frontend_v8.py

--- frontend.py
+++ frontend.py
@@ -9,25 +9,15 @@ from PIL import Image
 
 
 st.title("Pricing Data Visualizations")
-#st.text("Hello GeeksForGeeks!!!")
 tab1, tab2 = st.tabs(["Forward Pricing", "Settlement Pricing"])
 
-ora.init_oracle_client()
-cs = "localhost:1521/XE"
-#cs = "117.248.251.123:1521/XE"
-configpath = "C:\oraclexe\app\oracle\product\11.2.0\server\network\ADMIN"
 
-fwddates = getdata_v8.getData_DistinctFwdEntDate(cs, configpath)
-fwdcnames = getdata_v8.getData_DistinctFwdCurveName(cs, configpath)
-fwdphysfin = getdata_v8.getData_DistinctFwdPhysFin(cs, configpath)
-sptcnames = getdata_v8.getData_DistinctSpotCurveName(cs, configpath)
-sptfincurve = getdata_v8.getData_DistinctSpotFinCurve(cs,configpath)
-stdt, enddt = getdata_v8.getData_SpotStartEndDate(cs, configpath)
-
-"""def fetchfwddata(cs, configpath, curvename, date, physfin):
-    data = getdata_v7.getData_FwdPrice(cs, configpath, curvename, date, physfin)
-    graph_data = data.filter(['Curve Date', 'Price'])
-    print(graph_data)"""
+fwddates = getdata_v8.getData_DistinctFwdEntDate()
+fwdcnames = getdata_v8.getData_DistinctFwdCurveName()
+fwdphysfin = getdata_v8.getData_DistinctFwdPhysFin()
+sptcnames = getdata_v8.getData_DistinctSpotCurveName()
+sptfincurve = getdata_v8.getData_DistinctSpotFinCurve()
+stdt, enddt = getdata_v8.getData_SpotStartEndDate()
 
 
 with tab1:
@@ -50,53 +40,44 @@ with tab1:
             elif(fwdcurvename == "All" and fwddate == "None"):
                 st.error("Please select the Curve Name and the Date you want the graph to be generated from", icon="🚨")
             elif(fwdcurvename != "All" and fwddate != "None"):
-                st.toast('Graph will be generated soon', icon="ℹ️")
                 #st.write("Graph for ", fwdcurvename, "Noice")
-                data = getdata_v8.getData_SettPrice(cs, configpath, fwdcurvename, fwddate, fphysfin)
+                data = getdata_v8.getData_FwdPrice(fwdcurvename, fwddate, fphysfin)
                 graph_data = data.filter(['Curve Date', 'Price'])
                 print(graph_data)
                 if not graph_data.empty:
+                    st.toast('Graph will be generated soon', icon="ℹ️")
                     st.line_chart(data=graph_data, x='Curve Date', y='Price', use_container_width=True)
+                    st.toast('This is a success message!', icon="✅")
                 else:
                     st.error("No data available for the selected Curve name and Date", icon="🚨")
-                #st.write("getdata_v6.getData_FwdPrice has been called")
-                st.toast('This is a success message!', icon="✅")   
-
-        """st.write(stdt) 
-        st.write(enddt) """       
+                #st.write("getdata_v8.getData_FwdPrice has been called")
+   
 
 with tab2:
     with st.container():
         sptcurvename = st.selectbox("Spot Curve Names: (mandatory)", sptcnames)
         st.write("Curve Name selected is: ", sptcurvename)
 
-        startdate = st.date_input("Settlement Price Listing Start Date: (mandatory)", stdt)
+        startdate = st.date_input("Settlement Price Listing Start Date:", stdt)
         st.write("Report Date Selected is: ", startdate)
 
-        enddate = st.date_input("Settlement Price Listing End Date: (mandatory)", enddt)
-        st.write("Report Date Selected is: ", enddate)     
+        enddate = st.date_input("Settlement Price Listing End Date:", enddt)
+        st.write("Report Date Selected is: ", enddate)   
 
-        physfin = st.selectbox("Settlement Physical_Financial: (not mandatory)", sptfincurve)
-        st.write("Physical_Financial Curve Name selected is: ", physfin)
+        physfin = st.selectbox("Settlement(Spot) Financial Curve:", sptfincurve)
+        st.write("Physical_Financial Curve Name selected is: ", physfin)  
 
-        """if (st.button("Generate Graph", key='gengraph2', use_container_width=False)):
-            #st.write("Hello")
-            if(sptcurvename == "All" and date != "None"):
-                st.error("Please select a Curve Name", icon="🚨")
-            elif(sptcurvename != "All" and date == "None"):
-                st.error("Please select the Date you want the graph to be generated from", icon="🚨")
-            elif(sptcurvename == "All" and date == "None"):
-                st.error("Please select the Curve Name and the Date you want the graph to be generated from", icon="🚨")
-            elif(sptcurvename != "All" and date != "None"):
-                st.toast('Graph will be generated soon', icon="ℹ️")
-                #st.write("Graph for ", curvename, "Noice")
-                data = getdata_v8.getData_FwdPrice(cs, configpath, sptcurvename, date, physfin)
+        if (st.button("Generate Graph", key='gengraph2', use_container_width=False)):
+            if(sptfincurve == "All"):
+                st.error("Please select a Settlement Financial Curve", icon="🚨")
+            else:
+                data = getdata_v8.getData_SettPrice(sptcurvename, startdate, enddate, physfin)
                 graph_data = data.filter(['Curve Date', 'Price'])
                 print(graph_data)
                 if not graph_data.empty:
+                    st.toast('Graph will be generated soon', icon="ℹ️")
                     st.line_chart(data=graph_data, x='Curve Date', y='Price', use_container_width=True)
+                    st.toast('This is a success message!', icon="✅") 
                 else:
                     st.error("No data available for the selected Curve Name and Date Range", icon="🚨")
-                #st.write("getdata_v6.getData_FwdPrice has been called")
-                st.toast('This is a success message!', icon="✅") """           
-
+                #st.write("getdata_v8.getData_SettPrice has been called")
