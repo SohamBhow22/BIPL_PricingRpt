9c6063d7bbd2de255f0e096ae5c7798ac99febd3
Soham Bhowmick <32588339+SohamBhow22@users.noreply.github.com>
2023-09-09 19:50:53 +0530

    getdata_v3.py

--- getdata.py
+++ getdata.py
@@ -1,12 +1,12 @@
-import oracledb
-import datetime
+import oracledb as ora
+import datetime as dt
 import pandas as pd
 ## import db_config
 
 
 def getData_FwdPrice(cs,configpath):
     #ForwardPricing
-    con1 = oracledb.connect(user="FMS8TEST", password="FMS8TEST", dsn=cs, config_dir=configpath)
+    con1 = ora.connect(user="FMS8TEST", password="FMS8TEST", dsn=cs, config_dir=configpath)
     cur1 = con1.cursor()
 
     curvename = 'ICE_WIM'
@@ -19,38 +19,47 @@ def getData_FwdPrice(cs,configpath):
                         6:'Price', 7:'Ent_by', 8:'Ent_Date', 
                         9:'Aprv_by', 10:'Aprv_Date', 11: 'Flag'}, inplace=True)
     #print(data)
-    graph_data = data.filter(['Curve_Date', 'Curve_Name', 'Price'])
-    print(graph_data)
-
+    vizdata_fwd = data.filter(['Curve_Date', 'Curve_Name', 'Price'])
+    #print(vizdata_fwd)
+    
     cur1.close()
     con1.close()
+    return vizdata_fwd
 
 
 def getData_SettPrice(cs,configpath) :
     #SettlementPricing
-    con2 = oracledb.connect(user="FMS8TEST", password="FMS8TEST", dsn=cs, config_dir=configpath)
+    #con2 = ora.connect(user="FMS8TEST", password="FMS8TEST", dsn=cs, config_dir=configpath)
+    con2 = ora.connect(user="fms8", password="fms8", dsn=cs, config_dir=configpath)
     cur2 = con2.cursor()
 
-    curvename = 'DES_WES_INDIA_0_HALFMONTH'
+    curvename = 'PLATTS_JKM'
     query2 = "Select * from FMS9_CURVE2_PRICE_DTL where  ENT_DT >= to_date('01-01-2021','dd/mm/yyyy') and CURVE_NM = '"+ curvename + "' order by curve_dd_mm_yr asc"
 
     data = pd.DataFrame(cur2.execute(query2).fetchall())
-    data.rename(columns = {0:'Curve_Date', 1:'Curve_Name', 2:'Commodity_Type', 
+    print(data)
+    data.rename(columns = {0:'Curve Date', 1:'Curve_Name', 2:'Commodity_Type', 
                         3:'Curve_Type', 4:'Curve_Unit', 5:'Phys_Fin', 
                         6:'Price', 7:'Ent_by', 8:'Ent_Date', 
                         9:'Aprv_by', 10:'Aprv_Date', 11: 'Flag'}, inplace=True)
     #print(data)
-
-    graph_data = data.filter(['Curve_Date', 'Curve_Name', 'Price'])
-    print(graph_data)
-
+    vizdata_stl = data.filter(['Curve Date', 'Curve_Name', 'Price'])
+    print(vizdata_stl)
+    
     cur2.close()
     con2.close()
+    return vizdata_stl
 
 
 if __name__ == '__main__':
-    oracledb.init_oracle_client()
+    print("getdata_v3.py script has been called")
+    """ora.init_oracle_client()
     configpath = "C:\oraclexe\app\oracle\product\11.2.0\server\network\ADMIN"
     cs = "Soham-DellG15:1521/XE"
-    getData_FwdPrice(cs, configpath)
-    getData_SettPrice(cs, configpath)
+    cs = ''
+    configpath = ''
+    fwddata = getData_FwdPrice(cs, configpath)
+    stldata = getData_SettPrice(cs, configpath)
+    #print(fwddata)
+    #print(stldata)"""
+    print("getdata_v3.py script is running fine")
