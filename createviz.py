9f813dfe4ec0eacaad852a456eed37bb1a186db1
Soham Bhowmick <32588339+SohamBhow22@users.noreply.github.com>
2023-09-09 19:41:16 +0530

    Update createviz.py

--- createviz.py
+++ createviz.py
@@ -1,30 +1,30 @@
 import getdata_v3
 
 import oracledb as ora
-import matplotlib.pyplot as mpl_plt
-import matplotlib.dates as mpl_dates
+"""import matplotlib.pyplot as mpl_plt
+import matplotlib.dates as mpl_dates"""
+import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 def createviz(pricingname, data):
-    curvedate = pd.to_datetime(data['Curve_Date'], format='%Y%m%d')
+    curvedate = pd.to_datetime(data['Curve Date'], format='%Y%m%d')
     print(curvedate)
     #print(type(curvedate))
     curvename = data['Curve_Name'][0]
     #print(curvename)
     price = data['Price']
     print(price)
-        
-    mpl_plt.gca().xaxis.set_major_formatter(mpl_dates.DateFormatter('%d-%m-%Y'))
-    #mpl_plt.gca().xaxis.set_major_locator(mpl_dates.DayLocator())
-    mpl_plt.gca().xaxis.set_major_locator(mpl_dates.AutoDateLocator())
-    mpl_plt.plot_date(curvedate,price)
-    mpl_plt.gcf().autofmt_xdate()
-    mpl_plt.xlabel("Curve Date") 
-    mpl_plt.ylabel(pricingname + " Price (₹)") 
-    mpl_plt.title(pricingname + " Pricing Graph for " + curvename)
-    mpl_plt.show()
 
+    #plt = px.line(data, x='Curve_Date', y='Price', title = pricingname + " Pricing Graph for " + curvename, markers=True).update_layout(xaxis_title="Curve Date", yaxis_title=pricingname + " Price (₹)")
+    plt = px.line(data, x='Curve Date', y='Price', title = pricingname + " Pricing Graph for " + curvename, markers=True)
+    """plt = go.Figure(data = go.Scatter(x=curvedate, y=price))
+    plt.update_xaxes(title_text="Curve Date")
+    plt.update_yaxes(title_text=pricingname + " Price (₹)")"""
+    
+    plt.show()
 
+    
 if __name__ == '__main__':
     print()
     ora.init_oracle_client()
